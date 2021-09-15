bb1867f683aa46a990a2ebdd91e8d6892f45608b
peterAnami <peter.andake@student.moringaschool.com>
2021-09-15 14:13:28 +0300

    fast change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    QuoteComponent,
-    QuoteDetailComponent,
-    FormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
