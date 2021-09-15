da82ad2ce281416714a8ead9d5cc46dba8a1bc9a
peterAnami <peter.andake@student.moringaschool.com>
2021-09-15 14:48:01 +0300

    updates

---
+++ src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0,"","","",new Date());
+
+  
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
